3e417c37308eb6d71780ac81ec49660ebc213488
Philippe Marcelin <philippejrm@gmail.com>
2015-10-07 23:59:28 -0400

    display films

--- WebflixApplication/WebflixApplication/Controllers/HomeController.cs
+++ WebflixApplication/WebflixApplication/Controllers/HomeController.cs
@@ -14,8 +14,8 @@ namespace WebflixApplication.Controllers
             ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
 
             using(var webflixContext = new WebflixContext()){
-                //var films = webflixContext.FILMs.ToList();
-                return View(/*films*/);
+                var films = webflixContext.FILMs.Take(100).ToList();
+                return View(films);
             }
 
 
