8ff44c3bb6986b1633b49181254cdaeb44df4a2f
Philippe Marcelin <philippejrm@gmail.com>
2015-10-09 15:20:15 -0400

    Rent a movie (done)

--- WebflixApplication/WebflixApplication/Controllers/FilmController.cs
+++ WebflixApplication/WebflixApplication/Controllers/FilmController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebflixApplication.Models;
 
 namespace WebflixApplication.Controllers
 {
@@ -26,6 +27,32 @@ namespace WebflixApplication.Controllers
         public ActionResult SearchMovie(String query)
         {
             return Json(new { response = "success" });
+        }
+
+        public ActionResult RentMovie(int idFilm, int idClient)
+        {
+            CLIENT client;
+            COPIE copie;
+            FILM film;
+            using (var webflixContext = new WebflixContext())
+            {
+                client = webflixContext.CLIENTs.Find(idClient);
+                film = webflixContext.FILMs.Find(idFilm);
+                copie = film.COPIEs.Where(c => c.DISPONIBLE == true).First();
+                int clientMaxLoaction = (int)client.FORFAIT.LOCATIONSMAX;
+                int nombreLocationCourante = client.LOCATIONs.Count(l => l.DATERETOUR == null);
+                if (copie != null && nombreLocationCourante < clientMaxLoaction)
+                {
+                    LOCATION location = new LOCATION();
+                    location.CLIENT = client;
+                    copie.DISPONIBLE = false;
+                    location.COPIE = copie;
+                    location.DATEDELOCATION = DateTime.Now;
+                    webflixContext.LOCATIONs.Add(location);
+                    webflixContext.SaveChanges();
+                }
+                return RedirectToAction("ShowFilm", "Film", new { id = copie.IDFILM });
+            }
         }
     }
 }
