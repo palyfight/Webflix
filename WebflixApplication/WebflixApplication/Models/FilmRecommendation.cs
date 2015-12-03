50651a13254298746ec0c93a286a97d59b5fa3e4
Philippe Marcelin <philippejrm@gmail.com>
2015-12-02 21:55:01 -0500

    Recommandation

---
+++ WebflixApplication/WebflixApplication/Models/FilmRecommendation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebflixApplication.Models
+{
+    public class FilmRecommendation
+    {
+        public int id_film_J { get; set; }
+        public double correlation { get; set; }
+    }
+}
